99b9d24301174681632e0a102af99743b6d8ddaf
Charlotte Philippe <charlotteke.philippe@gmail.com>
2015-07-18 13:42:14 +0100

    Add reload option for dev

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.4.0-beta2"]
-                 [compojure "1.3.4"]]
+                 [compojure "1.3.4"]
+                 [hiccup "1.0.5"]]
   :main todo-list.core
   :min-lein-version "2.0.0"
   :uberjar-name "todo-list.jar"
