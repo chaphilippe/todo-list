1ae6ed9528bcae38316ad128ebe34943ba739684
Charlotte Philippe <charlotteke.philippe@gmail.com>
2015-07-18 12:05:43 +0100

    First commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject todo-list "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                [ring "1.4.0-beta2"]]
+  :main todo-list.core)
